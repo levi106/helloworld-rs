985509917b9046e1b0b47d97c1b788f1b3374e14
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2024-01-26 06:02:17 +0000

    implement web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{Request, Response};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+use http_body_util::Full;
+use std::{convert::Infallible, net::SocketAddr};
+
+async fn handle(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new("Ok".into())))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(handle))
+                .await
+            {
+                eprintln!("server error: {:?}", err);
+            }
+        });
+    }
 }
